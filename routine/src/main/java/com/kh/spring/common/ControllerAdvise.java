65f0fef06e16468a80c7e49f2f0b84e66112ab4d
OnggiJonggi <maru403@naver.com>
2025-07-03 23:54:50 +0900

    7.03_1

---
+++ routine/src/main/java/com/kh/spring/common/ControllerAdvise.java
@@ -0,0 +1,23 @@
+package com.kh.spring.common;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+
+import jakarta.servlet.http.HttpSession;
+
+
+@ControllerAdvice
+public class ControllerAdvise {
+//	@ModelAttribute("loginUser")
+//	public Member addLoginUser(HttpSession session) {
+//		if(session.getAttribute("loginUser") != null) {
+//			return (Member) session.getAttribute("loginUser");
+//		} else {
+//			return null;
+//		}
+//	}
+	
+}
