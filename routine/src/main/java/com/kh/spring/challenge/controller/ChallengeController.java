16b2f3d0ce2e536596244b9d8ad53b2548feb4f5
OnggiJonggi <maru403@naver.com>
2025-07-08 18:18:27 +0900

    7.8-1

--- routine/src/main/java/com/kh/spring/challenge/controller/ChallengeController.java
+++ routine/src/main/java/com/kh/spring/challenge/controller/ChallengeController.java
@@ -13,6 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.kh.spring.challenge.model.service.ChallengeService;
 import com.kh.spring.challenge.model.vo.Challenge;
+import com.kh.spring.challenge.model.vo.SearchChallenge;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -31,9 +32,9 @@ public class ChallengeController {
 	//비동기 - 챌린지 메인에서 챌린지 리스트
 	@ResponseBody
 	@PostMapping("/selectChal")
-	public String selectChal(HttpSession session, Model model, int currentPage) {
+	public String selectChal(HttpSession session, Model model, SearchChallenge sc) {
 		try {
-			service.selectChal(session,model,currentPage);
+			service.selectChal(session,model,sc);
 			return "true";
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -48,7 +49,7 @@ public class ChallengeController {
 		return "fragments :: chalDetail";
 	}
 
-	//새로운 챌린지 생성하기 - newChal조각 반환
+	//새로운 챌린지 생성하기
 	@GetMapping("/newChal")
 	public String getNewChal() {
 		return "fragments :: newChal";
@@ -70,13 +71,15 @@ public class ChallengeController {
 	@PostMapping("/newChal")
 	public String newChall(HttpSession session ,Model model
 			,Challenge chal ,List<MultipartFile> files) {
+		int chalNo=0;
 		try {
 			service.newChal(session, model, chal, files);
+			chalNo = chal.getChalNo();
 		} catch (Exception e) {
 			e.printStackTrace();
 			return "common/errorPage";
 		}
-		return "challenge/chalDetail";
+		return "redirect:/challenge/chalDetail?chalNo="+chalNo;
 	}
 	
 	//비동기 - 댓글 생성 중 사진 메타데이터 검증
